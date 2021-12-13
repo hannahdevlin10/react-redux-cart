c1d4ca7339fc253b5ddbd94acbb5dceab3af0576
Hannah Devlin <hannahdevlin@hannahs-mbp.home>
2021-12-13 16:41:16 +0000

    initial commit.

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,42 @@
+import React, { useState, useEffect } from "react";
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import { Link } from "react-router-dom";
+import styles from "./Navbar.module.css";
+
+import { connect } from "react-redux";
+
+const Navbar = ({ cart }) => {
+  const [cartCount, setCartCount] = useState(0);
+
+  useEffect(() => {
+    let count = 0;
+    cart.forEach((item) => {
+      count += item.qty;
+    });
+
+    setCartCount(count);
+  }, [cart, cartCount]);
+
+  return (
+    <div className={styles.navbar}>
+      <Link to="/">
+        <h2 className={styles.navbar__logo}>Redux Shopping Cart</h2>
+      </Link>
+      <Link to="/cart">
+        <div className={styles.navbar__cart}>
+          <h3 className={styles.cart__title}>Cart</h3>
+          <ShoppingCartIcon className={styles.cart__image} />
+          <div className={styles.cart__counter}>{cartCount}</div>
+        </div>
+      </Link>
+    </div>
+  );
+};
+
+const mapStateToProps = (state) => {
+  return {
+    cart: state.shop.cart,
+  };
+};
+
+export default connect(mapStateToProps)(Navbar);
